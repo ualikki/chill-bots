89b323c573b90de8494542e896754bd4ddcc4c48
PoIarStar <bogdanmurzintsev@yandex.ru>
2024-05-06 19:05:42 +0300

    fixed previous update

--- chill-bots/cheshire.py
+++ chill-bots/cheshire.py
@@ -3,9 +3,8 @@ from disnake.ext import commands
 import requests
 from config import keys
 
-
 bot = commands.Bot(command_prefix=commands.when_mentioned, intents=disnake.Intents.all(),
-                   test_guilds=[983432883714789476])
+                   test_guilds=[983432883714789476, 971007825218240532])
 
 
 @bot.event
@@ -30,31 +29,38 @@ async def url(inter, link: str):
     await inter.response.send_message(shortlink)
 
 
-@bot.slash_commands()
-@commands.dieffault_member_permissions(administrator=True)
+@bot.slash_command()
+@commands.default_member_permissions(administrator=True)
 async def ban(inter, user: disnake.Member, reason: str):
-    await user.send(embed=disnake.Embed(title='Уведомление',  description=f'Вас были заблокированы на сервере Чиллвилль администратором {inter.author}\
-        по причине {reason}. Вы можете подать аппеляцию, если не согласны с наказанием. Для этого необходимо заполнить форму: https://forms.gle/SDfNdS4R5cDE9VN7A'))
+    await user.send(
+        embed=disnake.Embed(title='Уведомление',
+                            description=f'Вас были заблокированы на сервере Чиллвилль администратором {inter.author} '
+                                        f'по причине "{reason}". Вы можете подать аппеляцию, если не согласны с '
+                                        f'наказанием. Для этого необходимо заполнить форму: '
+                                        f'https://forms.gle/SDfNdS4R5cDE9VN7A'))
     await user.ban()
 
-@bot.slash_commands()
-@commands.dieffault_member_permissions(administrator=True)
-async def ban(inter, user: disnake.Member):
+
+@bot.slash_command()
+@commands.default_member_permissions(administrator=True)
+async def kick(inter, user: disnake.Member):
     await user.kick()
 
 
-@bot.slash_commands()
-@commands.dieffault_member_permissions(administrator=True)
+@bot.slash_command()
+@commands.default_member_permissions(administrator=True)
 async def mute(inter, user: disnake.Member, hours: int = 0, minutes: int = 0, seconds: int = 0):
-    await user.timeout(hours * 3600 + minutes * 60 + seconds)
+    await user.timeout(duration=hours * 3600 + minutes * 60 + seconds)
 
-                    
+
 @bot.event
 async def on_slash_command_error(inter, error):
     if isinstance(error, commands.CommandOnCooldown):
         await inter.response.send_message(f'Попробуйте снова через {round(error.retry_after, 2)} сек.')
     else:
+        print(error)
         await inter.guild.get_channel(1209893365483573318).send(str(error))
         raise error
+
 
 bot.run(keys["cheshire"])
